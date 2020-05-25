bbc6e5d817c3935af4e3166a82394d8531e9fc93
Toni <a.saatze@googlemail.com>
2020-05-25 16:40:51 +0200

    cleared week fragment

--- app/app/src/main/java/edu/hm/foodweek/week/screen/WeekFragment.kt
+++ app/app/src/main/java/edu/hm/foodweek/week/screen/WeekFragment.kt
@@ -22,33 +22,6 @@ class WeekFragment : Fragment() {
     ): View? {
         viewModel = InjectorUtils.provideWeekViewModel(this)
         val root = inflater.inflate(R.layout.fragment_week, container, false)
-        setOnClicks(root)
-        setObservers(root)
         return root
-    }
-
-    private fun setObservers(root: View) {
-        viewModel.allMealPlans.observe(viewLifecycleOwner, Observer {
-            val sb = StringBuilder()
-            for (mealPlan in it) {
-                sb.append(mealPlan.toString())
-                sb.append("\n")
-            }
-            root.list_meals.text = sb.toString()
-        })
-        viewModel.allRecipes.observe(viewLifecycleOwner, Observer {
-            val sb = StringBuilder()
-            for (recipe in it) {
-                sb.append(recipe.toString())
-                sb.append("\n")
-            }
-            root.list_recipe.text = sb.toString()
-        })
-    }
-
-    private fun setOnClicks(root: View) {
-        root.btn_add_recipe.setOnClickListener {
-            viewModel.createRecipe()
-        }
     }
 }
