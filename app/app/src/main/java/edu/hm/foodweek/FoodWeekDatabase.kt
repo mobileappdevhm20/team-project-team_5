36996c0a40b3594b668c4f7a695f26376eee74c0
dcseifert <daniel.seifert@foundato.com>
2020-05-27 10:11:41 +0200

    Setup logic for initial load of db

--- app/app/src/main/java/edu/hm/foodweek/FoodWeekDatabase.kt
+++ app/app/src/main/java/edu/hm/foodweek/FoodWeekDatabase.kt
@@ -1,14 +1,20 @@
 package edu.hm.foodweek
 
 import android.content.Context
-import androidx.room.*
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import edu.hm.foodweek.plans.persistence.MealPlanDao
 import edu.hm.foodweek.plans.persistence.RecipeDao
-import edu.hm.foodweek.plans.persistence.model.*
+import edu.hm.foodweek.plans.persistence.model.MealPlan
 import edu.hm.foodweek.recipes.persistence.model.Recipe
 import edu.hm.foodweek.users.persistence.UserDao
 import edu.hm.foodweek.users.persistence.model.User
 import edu.hm.foodweek.util.Converters
+import java.util.concurrent.Executors
+
 
 @Database(
     entities = [MealPlan::class,User::class,Recipe::class],
@@ -31,13 +37,25 @@ abstract class FoodWeekDatabase : RoomDatabase() {
                         context.applicationContext,
                         FoodWeekDatabase::class.java,
                         "food_week_database"
-                    ).fallbackToDestructiveMigration().build()
+                    )
+                        .addCallback(object : RoomDatabase.Callback() {
+                            override fun onCreate(db: SupportSQLiteDatabase) {
+                                super.onCreate(db)
+                                //pre-populate data
+                                Executors.newSingleThreadExecutor().execute {
+                                    instance?.let {
+                                        // Insert data using daos
+                                        // -> it.recipeDao().createRecipe()...
+                                    }
+                                }
+                            }
+                        })
+                        .fallbackToDestructiveMigration().build()
                     INSTANCE = instance
                 }
                 return instance
             }
         }
-
     }
 }
 
