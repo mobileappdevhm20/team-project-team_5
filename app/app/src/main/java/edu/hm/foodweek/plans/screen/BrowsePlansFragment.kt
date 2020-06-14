c4e0bb3d57edfbb393123875c2039531c43e3bd2
hm-angelett <d.angelett@hm.edu>
2020-06-14 10:27:35 +0200

    Added onscrolllistener for recyclerview, needs proper testing!!!

--- app/app/src/main/java/edu/hm/foodweek/plans/screen/BrowsePlansFragment.kt
+++ app/app/src/main/java/edu/hm/foodweek/plans/screen/BrowsePlansFragment.kt
@@ -1,6 +1,7 @@
 package edu.hm.foodweek.plans.screen
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,16 +10,26 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import edu.hm.foodweek.R
 import edu.hm.foodweek.databinding.FragmentBrowsePlansBinding
+import edu.hm.foodweek.plans.persistence.model.MealPlan
+import edu.hm.foodweek.util.amplify.FoodWeekClient
+import edu.hm.foodweek.util.amplify.MealPlanResponse
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 /**
  * A simple [Fragment] subclass.
  */
-class BrowsePlansFragment : Fragment() {
+class BrowsePlansFragment : Fragment(), KoinComponent {
 
     private val mealPlanViewModel: MealPlanViewModel by viewModel()
+    private val foodWeekClient: FoodWeekClient by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,11 +43,13 @@ class BrowsePlansFragment : Fragment() {
         )
         binding.viewModel = mealPlanViewModel
 
-        binding.browsePlansSearchview.setOnQueryTextListener(object : SearchView.OnQueryTextListener, androidx.appcompat.widget.SearchView.OnQueryTextListener {
+        binding.browsePlansSearchview.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextChange(newText: String?): Boolean {
                 // inline this line, if you want to fire a call on each searchbar change
                 // mealPlanViewModel.filterText.postValue(newText)
-                if(newText.isNullOrEmpty()){
+                if (newText.isNullOrEmpty()) {
                     mealPlanViewModel.filterText.postValue("")
                 }
                 return true
@@ -63,6 +76,61 @@ class BrowsePlansFragment : Fragment() {
                 adapter.data = it
             }
         })
+
+        recyclerView.addOnScrollListener(object :
+            PlansEndlessScrollListener(LinearLayoutManager(this.context)) {
+            override fun loadMoreItems() {
+                loading = true
+                currentPage += 1
+                Log.println(
+                    Log.INFO,
+                    "BrowsePlansFragment",
+                    "isLoading? " + isLoading() + " currentPage " + currentPage
+                )
+                // Do API Call and update livedata
+                foodWeekClient.getFoodWeekServiceClient()
+                    .getMealPlans(currentPage, 20, binding.browsePlansSearchview.query.toString())
+                    .enqueue(object : Callback,
+                        retrofit2.Callback<MealPlanResponse> {
+                        override fun onFailure(call: Call<MealPlanResponse>, t: Throwable) {
+                            Log.println(
+                                Log.ERROR,
+                                "BrowsePlansFragment",
+                                "HTTP-Request /mealplans failed: ${t.message}"
+                            )
+                        }
+
+                        override fun onResponse(
+                            call: Call<MealPlanResponse>,
+                            response: Response<MealPlanResponse>
+                        ) {
+                            Log.println(
+                                Log.INFO,
+                                "BrowsePlansFragment",
+                                "HTTP-Request /mealplans for additional mealplans: ${response.code()}"
+                            )
+                            val mealPlanResponse = response.body()
+                            lastPage = mealPlanResponse!!.lastPage
+                            val foundMealPlans = response.body()?.mealPlans ?: emptyList()
+                            val currentMealPlans = ArrayList<MealPlan>()
+                            currentMealPlans.addAll(adapter.data)
+                            currentMealPlans.addAll(foundMealPlans)
+                            adapter.data = currentMealPlans
+                            adapter.notifyDataSetChanged()
+                        }
+                    })
+                loading = false
+            }
+
+            override fun isLastPage(): Boolean {
+                return lastPage
+            }
+
+            override fun isLoading(): Boolean {
+                return loading
+            }
+        })
+
         return binding.root
     }
 }
---
+++ app/app/src/main/java/edu/hm/foodweek/plans/screen/PlansEndlessScrollListener.kt
@@ -0,0 +1,31 @@
+package edu.hm.foodweek.plans.screen
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class PlansEndlessScrollListener(private val linearLayoutManager: LinearLayoutManager) :
+    RecyclerView.OnScrollListener() {
+
+    var lastPage: Boolean = false
+    var currentPage: Int = 0
+    var loading: Boolean = false
+
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+
+        val visibleItemCount = linearLayoutManager.childCount
+        val totalItemCount = linearLayoutManager.itemCount
+        val firstVisibleItemPosition = linearLayoutManager.findFirstVisibleItemPosition()
+
+        if(!isLoading() && !isLastPage()){
+            if((visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0){
+                loadMoreItems()
+            }
+        }
+    }
+
+    abstract fun loadMoreItems()
+    abstract fun isLastPage(): Boolean
+    abstract fun isLoading(): Boolean
+}
